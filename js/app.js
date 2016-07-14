3b5ce4f195d07e1d122934150a39270a9f5719de
etjones319 <etjones319@gmail.com>
2016-07-14 13:49:54 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
-$(document).foundation()
+$(document).foundation();
+$( document ).ready(function() {
+	$('form.multi-form a.next').click(function() {
+	  $('fieldset.active').removeClass('active').next('fieldset').addClass('active');
+	  $('div.active').addClass('inactive').removeClass('active').next('div').addClass('active').removeClass('inactive');
+	});
+
+	$('form.multi-form a.prev').click(function() {
+	  $('fieldset.active').removeClass('active').prev('fieldset').addClass('active');
+	  $('div.active').addClass('inactive').removeClass('active').prev('div').addClass('active').removeClass('inactive');
+	})
+});
