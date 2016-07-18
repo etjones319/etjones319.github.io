037c3039e6bf7517ea88b74e0fba7866bfc1ea74
etjones319 <etjones319@gmail.com>
2016-07-17 19:00:14 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 $(document).foundation();
 $(document).ready(function() {
+	//Toggle classes for Progress Bar and Next/Prev/Submit buttons
 	$('form.multi-form a.next').click(function() {
 	  $('fieldset.active').removeClass('active').next('fieldset').addClass('active');
 	  $('div.active').addClass('inactive').removeClass('active').next('div').addClass('active').removeClass('inactive');
 	});
-
 	$('form.multi-form a.prev').click(function() {
 	  $('fieldset.active').removeClass('active').prev('fieldset').addClass('active');
 	  $('div.active').addClass('inactive').removeClass('active').prev('div').addClass('active').removeClass('inactive');
