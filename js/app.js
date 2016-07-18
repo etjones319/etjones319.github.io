240818344ad847d92c584bf317743a5567cc7a66
etjones319 <etjones319@gmail.com>
2016-07-17 18:59:21 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 $(document).foundation();
-$( document ).ready(function() {
+$(document).ready(function() {
 	$('form.multi-form a.next').click(function() {
 	  $('fieldset.active').removeClass('active').next('fieldset').addClass('active');
 	  $('div.active').addClass('inactive').removeClass('active').next('div').addClass('active').removeClass('inactive');
